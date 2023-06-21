2b5f9ac458cceb08de594b84be63face6b55a63a
anjali-sim <anjalighetia123@gmail.com>
2023-06-21 10:40:46 +0530

    :wrench: Added the configuration file for the secure firebase data

---
+++ src/config/env.ts
@@ -0,0 +1,8 @@
+export const VITE_API_KEY = import.meta.env.VITE_APP_API_KEY;
+export const VITE_AUTH_DOMAIN = import.meta.env.VITE_APP_AUTH_DOMAIN;
+export const VITE_PROJECT_ID = import.meta.env.VITE_APP_PROJECT_ID;
+export const VITE_STORAGE_BUCKET = import.meta.env.VITE_APP_STORAGE_BUCKET;
+export const VITE_MESSAGING_SENDER_ID = import.meta.env
+  .VITE_APP_MESSAGING_SENDER_ID;
+export const VITE_ID = import.meta.env.VITE_APP_ID;
+export const VITE_MEASUREMENT_ID = import.meta.env.VITE_APP_MEASUREMENT_ID;
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,22 +2,29 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getDatabase, ref, onValue } from "firebase/database";
-// import firebase from 'firebase/compat/app';
+import {
+  VITE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_STORAGE_BUCKET,
+  VITE_MESSAGING_SENDER_ID,
+  VITE_ID,
+  VITE_MEASUREMENT_ID,
+} from "@/config/env";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDWN0MILp7GYkPrCZQB6nkHbZZZQIGeMIM",
-  authDomain: "url-shortener-app-9e46d.firebaseapp.com",
-  projectId: "url-shortener-app-9e46d",
-  storageBucket: "url-shortener-app-9e46d.appspot.com",
-  messagingSenderId: "498438820911",
-  appId: "1:498438820911:web:db2eaca2175cf1503462da",
-  measurementId: "G-ZW4LZPSZNT",
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_MESSAGING_SENDER_ID,
+  appId: VITE_ID,
+  measurementId: VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const database = getDatabase(app);
-// const db = firebase.firestore();
 
 export { app, auth, db, database, ref, onValue };
