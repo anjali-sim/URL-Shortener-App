95e07d1d62cca4f5b23d3227a454859091fc5b5e
anjali-sim <anjalighetia123@gmail.com>
2023-06-22 10:51:56 +0530

    :zap: Improved the code for the styling, service, etc.

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,18 @@ const App: React.FC = () => {
           <Route path="/view-profile" element={<Profile />} />
           <Route path="/edit-profile" element={<EditProfile />} />
           <Route path="/about" element={<About />} />
+
+{/* <PublicRoute restricted={false} path="/" element={<MainPage />} />
+          <PublicRoute restricted={true} path="/login" element={<Login />} />
+          <PublicRoute restricted={true} path="/signup" element={<SignUp />} />
+          <PublicRoute
+            restricted={true}
+            path="/forgot-password"
+            element={<ForgotPassword />}
+          />
+          <PrivateRoute path="/view-profile" element={<Profile />} />
+          <PrivateRoute path="/edit-profile" element={<EditProfile />} />
+          <PrivateRoute path="/about" element={<About />} /> */}
         </Routes>
       </BrowserRouter>
       </AppStyle>
@@ -32,3 +44,56 @@ const App: React.FC = () => {
 };
 
 export default App;
+
+
+
+// import React from "react";
+// import AppStyle from "@/styled/App.style.ts";
+// import MainPage from "@/pages/Main/MainPage";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Login from "@/pages/Login/Login";
+// import SignUp from "@/pages/SignUp/SignUp";
+// import ForgotPassword from "@/pages/ForgotPassword/ForgotPassword";
+// import Profile from "@/pages/UserProfile/ViewProfile.tsx";
+// import EditProfile from "@/pages/UserProfile/EditProfile.tsx";
+// import About from "@/pages/About/About.tsx";
+// import PublicRoute from "@/Routes/PublicRoute";
+// import PrivateRoute from "@/Routes/PrivateRoute";
+
+// const App: React.FC = () => {
+//   return (
+//     <AppStyle>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<MainPage />} />
+//           <PublicRoute
+//             restricted={true}
+//             path="/login"
+//             element={<Login />}
+//           />
+//           <PublicRoute
+//             restricted={true}
+//             path="/signup"
+//             element={<SignUp />}
+//           />
+//           <PublicRoute
+//             restricted={true}
+//             path="/forgot-password"
+//             element={<ForgotPassword />}
+//           />
+//           <PrivateRoute
+//             path="/view-profile"
+//             element={<Profile />}
+//           />
+//           <PrivateRoute
+//             path="/edit-profile"
+//             element={<EditProfile />}
+//           />
+//           <Route path="/about" element={<About />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </AppStyle>
+//   );
+// };
+
+// export default App;
--- src/components/Input/InputShortener.tsx
+++ src/components/Input/InputShortener.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Heading,
   InputContainer,
@@ -8,10 +8,28 @@ import {
   InputDiv,
   Paragraph,
   Image,
+  ResultDiv
 } from "./Input.style";
 import url from "@/assets/images/url.png";
+import axios from "axios";
 
 const InputShortener: React.FC = () => {
+  const [url, setUrl] = useState("");
+  const [shortenedUrl, setShortenedUrl] = useState("");
+
+  const handleShortenUrl = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get(
+        `https://api.shrtco.de/v2/shorten?url=${url}`
+      );
+      const data = response.data;
+      setShortenedUrl(data.result.full_short_link);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <InputContainer>
@@ -19,12 +37,24 @@ const InputShortener: React.FC = () => {
           SHORTENING YOUR <SpanStyle>URL?</SpanStyle>
         </Heading>
         <Paragraph>Sure, LinkZip will do that at a mouse click.</Paragraph>
-        <InputDiv>
-          <InputStyle type="text" placeholder="Paste a link to shorten it" />
-          <ButtonStyle>
-            <Image src={url} />
-          </ButtonStyle>
-        </InputDiv>
+        <form onSubmit={handleShortenUrl}>
+          <InputDiv>
+            <InputStyle
+              type="text"
+              placeholder="Paste a link to shorten it"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+            />
+            <ButtonStyle>
+              <Image src={url} />
+            </ButtonStyle>
+          </InputDiv>
+        </form>
+        {shortenedUrl && (
+          <ResultDiv>
+            <p>Shortened URL: {shortenedUrl}</p>
+          </ResultDiv>
+        )}
       </InputContainer>
     </>
   );
